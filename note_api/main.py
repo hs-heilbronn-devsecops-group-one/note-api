f55c0d6132881fae7f47f7e771808fc48f329b69
Bisma Saeed <bismasaeed00@gmail.com>
2024-12-08 18:54:45 +0000

    removed os import

--- note_api/main.py
+++ note_api/main.py
@@ -3,7 +3,6 @@ from uuid import uuid4
 from typing import List, Optional
 from os import getenv
 from typing_extensions import Annotated
-import os 
 
 from fastapi import Depends, FastAPI
 from opentelemetry import trace
@@ -14,7 +13,6 @@ from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from starlette.responses import RedirectResponse
 from .backends import Backend, RedisBackend, MemoryBackend, GCSBackend
 from .model import Note, CreateNoteRequest
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/workspaces/note-api/application_default_credentials.json'
 
 # Set up OpenTelemetry Tracer Provider
 trace.set_tracer_provider(TracerProvider())
