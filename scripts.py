3d472ea9ce46ad0de6db6085b90fda6d5b5dbcca
Bisma Saeed <bismasaeed00@gmail.com>
2024-12-09 11:55:06 +0000

    remove secret from printing

--- scripts.py
+++ scripts.py
@@ -4,6 +4,6 @@ import os
 my_secret = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
 
 if my_secret:
-    print(f"The secret is: {my_secret}")
+    print(f"The secret is added to the environment")
 else:
     print("MY_SECRET is not set in the environment.")
